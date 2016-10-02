b2663c152b6faa8a3ecd1f9c4344938c28989bc0
takarazima1995 <hitsujiniku.sugita@gmail.com>
2016-10-02 06:42:16 +0000

    change config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
